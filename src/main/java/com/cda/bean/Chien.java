f285f21390540251bf36d0a6dcbaae46044bf972
Fethi Benseddik <fe.benseddik@gmail.com>
2020-07-22 21:40:23 +0200

    creation crud chien ok

--- src/main/java/com/cda/bean/Chien.java
+++ src/main/java/com/cda/bean/Chien.java
@@ -1,4 +1,15 @@
 package com.cda.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Chien {
+    private int puceChien;
+    private String nom;
+    private String couleur;
+    private int age;
 }
---
+++ src/main/java/com/cda/service/impl/ChienServiceImp.java
@@ -0,0 +1,41 @@
+package com.cda.service.impl;
+
+import com.cda.bean.Chien;
+import com.cda.dao.IChienDao;
+import com.cda.service.IChienService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ChienServiceImp implements IChienService {
+
+
+    @Autowired
+    IChienDao iChienDaoDao;
+
+    @Override
+    public List<Chien> selectAll() {
+        return iChienDaoDao.selectAll();
+    }
+
+    @Override
+    public void deleteById(Chien p) {
+        iChienDaoDao.deleteById(p);
+    }
+
+    @Override
+    public Chien findById(int id) {
+        return iChienDaoDao.findById(id);
+    }
+
+    @Override
+    public void create(Chien p) {
+        iChienDaoDao.create(p);
+    }
+
+    @Override
+    public void update(Chien p) {
+        iChienDaoDao.update(p);
+    }
+}
